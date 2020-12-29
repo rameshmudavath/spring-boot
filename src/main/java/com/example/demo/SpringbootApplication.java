e8e6cdd0b1d00011789f39d3768afbd7d744e129
Ramesh mudavath <rameshmahi1996@gmail.com>
2020-12-29 15:09:01 +0530

    main method modified

--- src/main/java/com/example/demo/SpringbootApplication.java
+++ src/main/java/com/example/demo/SpringbootApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		ConfigurableApplicationContext run=SpringApplication.run(SpringbootApplication.class, args);
+		
+		run.close();
+		
 	}
 
 }
